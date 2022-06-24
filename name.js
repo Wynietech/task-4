1f71b4ff5be331e27e019ae38b3247830766700c
Wynietech <105459074+Wynietech@users.noreply.github.com>
2022-06-24 14:20:40 +0100

    Add files via upload

---
+++ name.js
@@ -0,0 +1,11 @@
+var name = "My name is  Aimiede Winnie Lugard-Agege"
+var height = "I am 5ft 5inches"
+var nationality = "I am a Nigerian"
+var tu = "Thank you zuri!"
+alert(name);
+
+
+alert(height);
+alert(nationality);
+alert(tu);
+
